2eea75fa7563fc4ab0c6ed75c617565c29d88c59
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-05-09 21:33:55 +1000

    Added mailgun for email authorisation when signing up, also added a gmail access method which is hashed out as backup. Also see the new planning board updates...

--- src/marshome/config/environments/development.rb
+++ src/marshome/config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -60,4 +60,36 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mailgun.org',
+    port: 587,
+    enable_starttls_auto: true,
+    authentication: 'plain',
+    user_name: 'postmaster@sandboxb4cbcba707de461fa11c2039b885f75e.mailgun.org',
+    password: '1eac404baf9ea88a4272d4ef9002ca67-65b08458-322ad992'
+  }
+
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   enable_starttls_auto: true,
+  #   authentication: 'plain',
+  #   user_name: 'marsbnb.contact@gmail.com',
+  #   password: 'marsbnb1234A'
+  # }
+
+config.paperclip_defaults = {
+  storage: :s3,
+  path: ':class/:attachment/:id/:style/:filename',
+  s3_host_name: 's3-ap-southeast-2.amazonaws.com',
+  s3_credentials: {
+    bucket: 'marsr32chris',
+    access_key_id: 'AKIAWKS64OPA7N7FZS7A',
+    secret_access_key: '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',
+    s3_region: 'ap-southeast-2'
+  }
+}
+
 end
--- src/marshome/db/schema.rb
+++ src/marshome/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_001320) do
+ActiveRecord::Schema.define(version: 2020_05_09_104832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,11 +25,11 @@ ActiveRecord::Schema.define(version: 2020_05_09_001320) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.datetime "date_joined"
-    t.string "phone"
-    t.string "profile_picture"
-    t.string "listing"
-    t.string "message"
+    t.string "date_joined"
+    t.integer "phone"
+    t.integer "profile_picture"
+    t.integer "listing"
+    t.integer "message"
     t.float "rating_total"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
