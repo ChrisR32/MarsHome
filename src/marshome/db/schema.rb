db6a1ebc30274537426b3f52d8172616cdd12f90
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-05-10 22:17:49 +1000

    added facebook sign in with omniaurth, and as normal a screenshoot of planning

--- src/marshome/db/schema.rb
+++ src/marshome/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_055038) do
+ActiveRecord::Schema.define(version: 2020_05_10_101719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 2020_05_10_055038) do
     t.string "provider"
     t.string "uid"
     t.string "image_fb"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.text "description"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
