aad9e8cc228aa266a26103657044c72d43d4fe3c
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-05-11 15:43:16 +1000

    fix to db issue

--- src/marshome/app/models/user.rb
+++ src/marshome/app/models/user.rb
@@ -23,6 +23,7 @@ def self.from_omniauth(auth)
         user.first_name = auth.info.first_name   # assuming the user model has a name
         user.last_name = auth.info.last_name   # assuming the user model has a name
         user.image_fb = auth.info.image_fb # assuming the user model has an image
+
         user.uid = auth.uid
         user.provider = auth.provider
         # If you are using confirmable and the provider(s) you use validate emails, 
