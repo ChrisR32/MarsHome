b5e69fd2e29dcaf4cddb9a41a1e06cd6eb204a65
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-05-11 13:50:18 +1000

    added photo model, photo contoller, photo view, aws-s3. also fixed a db:migration conflict that was preventing the app from migrating

--- src/marshome/app/controllers/photos_controller.rb
+++ src/marshome/app/controllers/photos_controller.rb
@@ -1,23 +1,25 @@
 class PhotosController < ApplicationController
-
+def photo_upload
+render text: "uploading"
+end
     def create
-        @room = Room.find(params[:room_id])
+        @listing = Listing.find(params[:listings_id])
 
-        if params[:images]
-            params[:images].each do |img|
-                @room.photos.create(image: img)
-            end
-            @photos = @room.photos
-            redirect_back(fallback_location: request.referer, notice: "Saved....")
-        end
+        # if params[:images]
+        #     params[:images].each do |img|
+        #         @listing.photos.create(image: img)
+        #     end
+        #     @photos = @listing.photos
+        #     redirect_back(fallback_location: request.referer, notice: "Saved....")
+        # end
     end
 
     def destroy
         @photo = Photo.find(params[:id])
-        room = @photo.room
+        listing = @photo.listing
 
         @photo.destroy
-        @photos = Photo.where(room_id: room.id)
+        @photos = Photo.where(listings_id: listings.id)
 
         respond_to :js
     end
