7687be8a2fe483c3eea7ae299cd58a62dfb0a830
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-05-11 15:43:16 +1000

    fix to db issue

--- src/marshome/app/controllers/photos_controller.rb
+++ src/marshome/app/controllers/photos_controller.rb
@@ -1,23 +1,23 @@
 class PhotosController < ApplicationController
 
     def create
-        @room = Room.find(params[:room_id])
+        @listing = Listing.find(params[:listing_id])
 
         if params[:images]
             params[:images].each do |img|
-                @room.photos.create(image: img)
+                @listing.photos.create(image: img)
             end
-            @photos = @room.photos
+            @photos = @listing.photos
             redirect_back(fallback_location: request.referer, notice: "Saved....")
         end
     end
 
     def destroy
         @photo = Photo.find(params[:id])
-        room = @photo.room
+        listing = @photo.listing
 
         @photo.destroy
-        @photos = Photo.where(room_id: room.id)
+        @photos = Photo.where(listing_id: listing.id)
 
         respond_to :js
     end
