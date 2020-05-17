e466d21232a1338a6df897ddc77adbb75d05c1ad
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-05-17 14:46:07 +1000

    added avatars that the user can pick from in the edit menu

--- src/marshome/app/helpers/application_helper.rb
+++ src/marshome/app/helpers/application_helper.rb
@@ -1,5 +1,29 @@
 module ApplicationHelper
     def avatar_url(user)
-        "/slide/default_user5.png"
+       if user.profile_picture == 'Alien 2'
+        "/avatars/a2.png"
+       elsif user.profile_picture == 'Alien 3'
+        "/avatars/a3.png"
+    elsif user.profile_picture == 'Alien 4'
+        "/avatars/a4.png"
+    elsif user.profile_picture == 'Alien 5'
+        "/avatars/a5.png"
+    elsif user.profile_picture == 'Alien 6'
+        "/avatars/a6.png"
+    elsif user.profile_picture == 'Alien 7'
+        "/avatars/a7.png"
+    elsif user.profile_picture == 'Alien 8'
+        "/avatars/a8.png"
+    elsif user.profile_picture == 'Alien 9'
+        "/avatars/a9.png"
+    elsif user.profile_picture == 'Alien 10'
+        "/avatars/a10.png"
+    elsif user.profile_picture == 'Alien 11'
+        "/avatars/a11.png"
+    elsif user.profile_picture == 'Alien 12'
+        "/avatars/a12.png"
+    else
+        "/avatars/a1.png"
+       end
      end
 end
